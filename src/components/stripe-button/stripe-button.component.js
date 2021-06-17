33213396423e476c36203a839ab955d35a82b035
AnnaKupryushina <anna@Annas-MacBook-Pro.local>
2021-06-17 22:31:30 +0300

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51J3PDQAbVcVA0D7NMsq3O4yOLSPvkKFfKQxjnd6C3dXQiiPIugMkH7U2VafFUc9CMaykz9cxOr3MwJ9atMOUEx4L00zJNwBDzSselectCollectionsForPreview'
+
+const onToken= token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey} 
+        />
+    );
+};
+
+export default StripeCheckoutButton;
+
