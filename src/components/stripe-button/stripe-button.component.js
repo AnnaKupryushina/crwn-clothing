b2b5c09f96d91126f0ca2b3ae9e491502bcab965
AnnaKupryushina <anna@Annas-MacBook-Pro.local>
2021-06-19 20:38:59 +0300

    removed redux-logger

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
+    //const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51J3PDQAbVcVA0D7NMsq3O4yOLSPvkKFfKQxjnd6C3dXQiiPIugMkH7U2VafFUc9CMaykz9cxOr3MwJ9atMOUEx4L00zJNwBDzSselectCollectionsForPreview'
 
 const onToken= token => {
